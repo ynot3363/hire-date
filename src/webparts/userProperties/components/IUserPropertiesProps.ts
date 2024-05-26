e8c4360c501d633bb147a9e2f1af1cf2cdc17cba
Anthony Poulin <ynot3363@gmail.com>
2024-05-26 11:42:42 -0400

    Initial Scaffold

---
+++ src/webparts/userProperties/components/IUserPropertiesProps.ts
@@ -0,0 +1,7 @@
+export interface IUserPropertiesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/userProperties/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IUserPropertiesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'UserPropertiesWebPartStrings' {
+  const strings: IUserPropertiesWebPartStrings;
+  export = strings;
+}
